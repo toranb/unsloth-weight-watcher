e7098466f7ab81d5e6ec799b47275862989132e5
toran billups <toranb@gmail.com>
2024-02-24 12:33:39 -0600

    working alpha of mistral and fine tune

--- demo.py
+++ demo.py
@@ -1,5 +1,17 @@
 import weightwatcher as ww
+import matplotlib.pyplot as plt
+import pandas as pd
 
 watcher = ww.WeightWatcher()
-details = watcher.analyze(model='model', savefig='yolo')
-details.alpha.plot.hist()
+details = watcher.analyze(model='model',fix_fingers='clip_xmax')
+base_details = watcher.analyze(model='base',fix_fingers='clip_xmax')
+
+print(details.alpha.mean())
+details.to_csv('filename.csv', index=False)
+
+details.alpha.plot.hist(bins=100, color='red', alpha=0.5, density=True, label='finetune')
+base_details.alpha.plot.hist(bins=100, color='green', density=True, label='mistral')
+plt.legend()
+plt.xlabel(r"alpha $(\alpha)$ PL exponent")
+plt.title(r"Mistral fine tune layer alphas $(\alpha)$")
+plt.savefig("alphas.png")
